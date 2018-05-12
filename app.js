c79626d7b2376928b904267e19ec407871060cfa
yoga <y.wang355@student.unimelb.edu.au>
2018-05-12 22:05:00 +1000

    back end data updated

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 3001;
+
+// Database setup
+require('./models/db.js');
 
 app.use(express.static('public'));
 app.use("/styles",express.static(__dirname + "/styles"));
 app.use("/controllers",express.static(__dirname + "/controllers"));
 app.set('view engine', 'ejs');
 
+// Routes setup
 const router = require('./routes/routes');
 
 app.use(router);
